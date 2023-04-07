99791efb9bd354e1167fee10c3c57118d8d9aa20
Michael Michelotti <50560169+michael-michelotti@users.noreply.github.com>
2023-04-07 15:35:01 -0700

    commit changes for push to github

--- main.py
+++ main.py
@@ -3,9 +3,14 @@
 # module, explaining the module
 
 import pathlib
+import re
 
 # General steps:
 # Initialization - create write buffer, write project overview to write buffer
+# Establish ignore list
+with open("prompt_ignore") as fid:
+    ignore_list = [entry.strip() for entry in fid.readlines()]
+
 project_root_dir = pathlib.Path.cwd()
 out_file_path = pathlib.Path(f"{project_root_dir.name}_gpt_prompt.txt")
 out_buff = open(out_file_path, "w")
@@ -17,12 +22,27 @@ out_buff.write(f"The entry point to my project is called {entry_point_file.name}
 with open(entry_point_file) as fid:
     out_buff.write(fid.read())
 
-# Begin iteration over all files and directories
-for file in project_root_dir.iterdir():
-    # Filter out files or directories that should be ignored
-    if file.name == entry_point_file.name or file.name == out_file_path.name or file.name == ".git":
-        continue
 
+def filter_file_list(file_iter, ignore_list):
+    # Find all files at root level, remove with filter criteria
+    file_list = []
+    for file in project_root_dir.iterdir():
+        for ignore_string in ignore_list:
+            if re.search(ignore_string, file.name):
+                continue
+        # Filter out files or directories that should be ignored
+        if file.name == entry_point_file.name or file.name == out_file_path.name:
+            continue
+
+        file_list.append(file)
+
+    return file_list
+
+
+file_list = filter_file_list(project_root_dir.iterdir(), ignore_list)
+
+# Begin iteration over all files and directories
+for file in file_list:
     # If the file is a directory, enter directory and begin recursively iterating
     if file.is_dir():
         out_buff.write(f"My project has a directory called {file.name}.\n")
@@ -30,6 +50,7 @@ for file in project_root_dir.iterdir():
             out_buff.write(f"That directory has a file called {file.name} with the following text:\n")
             with open(f2) as f2id:
                 out_buff.write(f2id.read())
+        continue
 
     # Write file context to write buffer
     out_buff.write(f"My project has a file called {file.name} with the following text:\n")
---
+++ test/__init__.py
@@ -0,0 +1,5 @@
+"""test module with a docstring"""
+
+
+def l1_module_fn(c, d):
+    return c + d
