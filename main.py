fa093be4f0687582975f062fb2700ffb0b2aaf47
Michael Michelotti <50560169+michael-michelotti@users.noreply.github.com>
2023-04-07 17:52:22 -0700

    fix filter file list function to break if ignore match is found

--- main.py
+++ main.py
@@ -4,6 +4,9 @@
 
 import pathlib
 import re
+import os
+
+LOG_MODE = "print"
 
 # General steps:
 # Initialization - create write buffer, write project overview to write buffer
@@ -14,23 +17,33 @@ with open("prompt_ignore") as fid:
 project_root_dir = pathlib.Path.cwd()
 out_file_path = pathlib.Path(f"{project_root_dir.name}_gpt_prompt.txt")
 out_buff = open(out_file_path, "w")
-out_buff.write(f"My project is called {project_root_dir.name}\n")
+
+if LOG_MODE == "print":
+    print_fn = print
+else:
+    print_fn = out_buff.write
+
+print_fn(f"My project is called {project_root_dir.name}\n")
 
 # Start by writing entry point context to output buffer
 entry_point_file = pathlib.Path(__file__)
-out_buff.write(f"The entry point to my project is called {entry_point_file.name}. Here is the full text\n")
+print_fn(f"The entry point to my project is called {entry_point_file.name}. Here is the full text\n")
 with open(entry_point_file) as fid:
-    out_buff.write(fid.read())
-
+    print_fn(fid.read())
 
 def filter_file_list(file_iter, ignore_list):
     # Find all files at root level, remove with filter criteria
+    match_flag = False
     file_list = []
+
     for file in project_root_dir.iterdir():
         for ignore_string in ignore_list:
             if re.search(ignore_string, file.name):
-                continue
-        # Filter out files or directories that should be ignored
+                match_flag = True
+                break
+        if match_flag:
+            match_flag = False
+            continue
         if file.name == entry_point_file.name or file.name == out_file_path.name:
             continue
 
@@ -38,28 +51,29 @@ def filter_file_list(file_iter, ignore_list):
 
     return file_list
 
+def process_directory(dir_root):
+    """Process all files in a given directory. If a directory is encountered, call recursively"""
+    entry_list = filter_file_list(dir_root.iterdir(), ignore_list)
+    for entry in entry_list:
+        if entry.is_file():
+            print_fn(f"File found: {entry.name}")
+        elif entry.is_dir():
+            print_fn(f"==== Directory Found: {entry.name} ======")
+            process_directory(entry)
 
-file_list = filter_file_list(project_root_dir.iterdir(), ignore_list)
 
-# Begin iteration over all files and directories
-for file in file_list:
-    # If the file is a directory, enter directory and begin recursively iterating
-    if file.is_dir():
-        out_buff.write(f"My project has a directory called {file.name}.\n")
-        for f2 in file.iterdir():
-            out_buff.write(f"That directory has a file called {file.name} with the following text:\n")
-            with open(f2) as f2id:
-                out_buff.write(f2id.read())
-        continue
+process_directory(project_root_dir)
 
-    # Write file context to write buffer
-    out_buff.write(f"My project has a file called {file.name} with the following text:\n")
-    with open(file) as fid:
-        out_buff.write(fid.read())
+
+# Write file context to write buffer
+# print_fn(f"My project has a file called {file.name} with the following text:\n")
+# with open(file) as fid:
+#     out_buff.write(fid.read())
 
 # Write intermediate buffer to final buffer
 # Right now, only buffer is the output file
 
 # Cleanup (write closing context, close file)
-out_buff.write("Try to convince me that this script could be improved, and provide examples.\n")
+print_fn("Try to convince me that this script could be improved, and provide examples.")
+# out_buff.write("Try to convince me that this script could be improved, and provide examples.\n")
 out_buff.close()
